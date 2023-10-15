7a747e80d6e662ad36062473378a063afcf2df17
Timur-Malik <92898560+tim-fihost@users.noreply.github.com>
2023-10-15 18:43:30 +0900

    Update inform.py

--- inform.py
+++ inform.py
@@ -2,14 +2,14 @@ import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-my_email = os.environ['EMAIL']
-my_password = os.environ['G_PW']
-recipient_email = "timuleooo0212@gmail.com"
+my_email = os.environ['EMAIL'] #Clean this value and paste your
+my_password = os.environ['G_PW'] #Clean this value and paste your
+recipient_email = "YOUR_EMAIL"
 def send_email(price, link):
         with smtplib.SMTP('smtp.gmail.com', 587) as server: 
                 msg = MIMEMultipart()
                 msg['From'] = my_email
-                msg['To'] = "timuleooo0212@gmail.com"
+                msg['To'] = recipient_email
                 msg['Subject'] = "Hello Check Toaday's Price"
                 body = f"""Good day!
 We have a great news check the price today for your item.
